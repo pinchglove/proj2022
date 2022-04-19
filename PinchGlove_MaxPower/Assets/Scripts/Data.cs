f257b7a4fdf274471fb99371570e98899fc8da68
jay1261 <dongjae73@naver.com>
2022-04-19 21:31:05 +0900

    First Santa Project

---
+++ PinchGlove_MaxPower/Assets/Scripts/Data.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    public string userID;
+    public string userName;
+    public string date;
+
+    public static Data instance = null;
+
+    //singletone
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if(instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
