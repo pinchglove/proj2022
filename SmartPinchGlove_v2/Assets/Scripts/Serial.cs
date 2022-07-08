080040008d9b888f65133789a9e34049883f67f2
jay1261 <dongjae73@naver.com>
2022-07-08 14:11:35 +0900

    Optimized Serial.cs

--- SmartPinchGlove_v2/Assets/Scripts/Serial.cs
+++ SmartPinchGlove_v2/Assets/Scripts/Serial.cs
@@ -67,7 +67,7 @@ public class Serial : MonoBehaviour
         //SerialSendingStop(); // 연결된 이후 게임 플레이 전까지 데이터 전송 stop시켜놓음
     }
 
-    public void ConnectSerial()
+    public void ConnectSerial() // 검색해서 자동 연결
     {
         string[] ports = SerialPort.GetPortNames();
         foreach (string p in ports)
@@ -108,13 +108,12 @@ public class Serial : MonoBehaviour
         }
     }
     
+
+
     private void MySerialReceived()  //데이터 가공
     {
-        if(queue.Count > 5)
-        {
-            queue.Clear();
-        }
-        // 큐에 넣고 빼기
+
+       // 인큐 과정
        string tmp = sp.ReadExisting(); //업데이트 마다 현재 입력 버퍼에서 가져옴
        splitedTmp = tmp.Split('\n');   // 가져온거 줄바꿈 기준으로 잘라서 배열에 넣음
        foreach (string s in splitedTmp)    //잘라서 넣은 배열하나씩 돌면서 빈 배열이 아니면 queue에 넣어줌
@@ -122,29 +121,11 @@ public class Serial : MonoBehaviour
            if (s != "")
            {
                queue.Enqueue(s.Replace("\r",""));
+                GetDataFromQueue();     // foreach 반복문에 디큐 과정을 같이 넣어서 큐가 쌓이는 상황 해결
            }
        }
 
-       if (queue.Any())    //큐가 비어있지 않으면
-       {
-           if (str == "")  //str 초기화 상태인지 확인
-               str = queue.Dequeue();  // queue에서 빼서 str에 넣어줌
-           else if (!str.Contains('a'))    // 현재 조건에서 str의 맨 처음에 a가 없을 경우 받아온 데이터가 깨진 상태일 것으로 예상 
-           {
-               Debug.Log("a없음:" + str);
-               str = "";
-           }
-           if (str.Contains('a') && str.Contains('b'))  //str이 a와 b를 포함하고 있으면 
-           {                                                            // "a,0000,0000,0000,0000,0000,b\r" 이 형식이 필요한데 \r은 버려도 됨
-               str = str.Replace('a','2').Replace('b', '3');            //a를2로, b를3으로 바꿔주고 데이터 처리  a를 버리고 처리할까? 흠 굳이 이긴 한데 그럼 조금더 빠르긴 할듯?
-               datareceive();  //데이터 처리
-           }
-           else if(queue.Any())    //위 if문 처럼 완벽한 포멧을 갖추고 있지 않으면 && queue가 비어있지 않으면 -> str에 queue에서 빼서 더해줌, 이후 다시 돌면서 포멧 갖춰지면 데이터 처리됨
-           {
-               str += queue.Dequeue();
-           }
 
-       }
         
          /*//아래는 a,b로 구분이 아닌, 2,3으로 들어올 때 "2,~~~~,3\r\n" 문자 구분이 안되서 길이로 구분했던 코드 - 에러가 좀 있다.
         print("=====================================================================");
@@ -208,7 +189,30 @@ public class Serial : MonoBehaviour
         */
     }
 
-    void datareceive()
+    void GetDataFromQueue() //디큐 과정
+    {
+        if (queue.Any())    //큐가 비어있지 않으면
+        {
+            if (str == "")  //str 초기화 상태인지 확인
+                str = queue.Dequeue();  // queue에서 빼서 str에 넣어줌
+            else if (!str.Contains('a'))    // 현재 조건에서 str의 맨 처음에 a가 없을 경우 받아온 데이터가 깨진 상태일 것으로 예상 
+            {
+                Debug.Log("a없음:" + str);
+                str = "";
+            }
+            if (str.Contains('a') && str.Contains('b'))  //str이 a와 b를 포함하고 있으면 
+            {                                                            // "a,0000,0000,0000,0000,0000,b\r" 이 형식이 필요한데 \r은 버려도 됨
+                str = str.Replace('a', '2').Replace('b', '3');            //a를2로, b를3으로 바꿔주고 데이터 처리  a를 버리고 처리할까? 흠 굳이 이긴 한데 그럼 조금더 빠르긴 할듯?
+                datareceive();  //데이터 처리
+            }
+            else if (queue.Any())    //위 if문 처럼 완벽한 포멧을 갖추고 있지 않으면 && queue가 비어있지 않으면 -> str에 queue에서 빼서 더해줌, 이후 다시 돌면서 포멧 갖춰지면 데이터 처리됨
+            {
+                str += queue.Dequeue();
+            }
+        }
+    }
+
+    void datareceive() 
     {
         tempstr = str.Split(','); // , 단위로 나눠서 배열에 순서대로 저장
         
@@ -232,6 +236,12 @@ public class Serial : MonoBehaviour
         Inputdata.start = data[0];
         str = "";
     }
+
+
+
+
+
+
 
     public void SerialSendingStart()
     {
